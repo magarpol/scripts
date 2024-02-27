5d3442ac20daf52eb2a1862254c31a33205bed74
magarpol <159026290+magarpol@users.noreply.github.com>
2024-02-27 07:26:18 +0100

    delete_username_folder.sh

--- delete_username_folder.sh
+++ delete_username_folder.sh
@@ -3,17 +3,17 @@
 #Prompt the username
 read -p "Enter the folder to delete: " username
 
-#Check if the username folder exists in /mid/server/scratch/
-if [ -d "/mid/server/scratch/$username" ]; then
+#Check if the username folder exists in /path/
+if [ -d "/path/$username" ]; then
 	#Confirm before deleting
-	read -p "The folder /mid/server/scratch/$username exists. Do you want to delete it? (y/n):" confirm
+	read -p "The folder /path/$username exists. Do you want to delete it? (y/n):" confirm
 	if [ "$confirm" = "y" ]; then
 		#Delete the folder and its contents
-		rm -rf "/mid/server/scratch/$username"
-		echo "The folder /mid/server/scratch/$username and its contents have been deleted."
+		rm -rf "/path/$username"
+		echo "The folder /path/$username and its contents have been deleted."
 	else
 		echo "Folder deletion canceled"
 	fi
 else
-	echo "The folder /mid/server/scratch/$username does not exist."
+	echo "The folder /path/$username does not exist."
 fi
