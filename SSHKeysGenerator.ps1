9720a4c4b8d2f7767ccbef88da0b67e322dfcde8
magarpol <159026290+magarpol@users.noreply.github.com>
2024-05-23 09:39:24 +0200

    SSHKeysGenerator.ps1
    Added a 4th step for stopping the ssh-agent

--- SSHKeysGenerator.ps1
+++ SSHKeysGenerator.ps1
@@ -3,10 +3,12 @@ ssh-keygen -t rsa -b 4096 -f "$env:USERPROFILE\.ssh\id_rsa" -N ""
 
 # Step 2: Start the SSH agent
 Start-Service ssh-agent
-Get-Service ssh-agent | Set-Service -StartupType Automatic
 
 # Step 3: Add SSH key to the SSH agent
 ssh-add "$env:USERPROFILE\.ssh\id_rsa"
 
-# Step 4: Copy Your Public Key To Your Clipboard
+# Step 4: Stop the SSH agent
+Stop-Service ssh-agent
+
+# Step 5: Copy Your Public Key To Your Clipboard
 notepad "$env:USERPROFILE\.ssh\id_rsa.pub"
