efaa3f93ed9d03b43ec10c18d5a5a6a5d1fcd417
magarpol <159026290+magarpol@users.noreply.github.com>
2024-05-28 13:20:55 +0200

    Mount NFS shares on openSUSE 15.5

---
+++ NFSShares.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# User input for mount directory
+mount_directory() {
+    read -p "Enter the directory where NFS shares should be mounted (default is /mnt): " mount_dir
+    if [[ -z "$mount_dir" ]]; then
+        mount_dir="/mnt"
+    fi
+    echo "Mount directory: $mount_dir"
+}
+
+# User input for NFS mount points
+user_directory() {
+    read -p "Enter the name for the directory under $mount_dir: " user_input
+    related_mount_dir="$mount_dir/$user_input"
+    mkdir -p "$related_mount_dir"
+    echo "Directory created: $related_mount_dir"
+}
+
+# User input for NFS shares path
+get_nfs_shares() {
+    echo "Enter the NFS share to be mounted, for example: 'qnap3:/na1/backup_na1/galadriel/export1/share/sun1' one by one"
+    echo "When done, press Enter"
+    nfs_shares=()
+    while true; do
+        read -p "> " input
+        if [[ -z "$input" ]]; then
+            break
+        fi
+        nfs_shares+=("$input")
+    done
+}
+
+# Update /etc/fstab
+update_fstab() {
+    for share in "${nfs_shares[@]}"; do
+        echo "$share $related_mount_dir  nfs  rw  0  0" |  tee -a /etc/fstab
+    done
+}
+
+# Mount NFS shares
+mount_shares() {
+    mount -a
+    echo "NFS shares mounted"
+	ls "$related_mount_dir"
+}
+
