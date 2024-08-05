24df9518653273c62622b320e841db4d715f873b
Mauro García <159026290+magarpol@users.noreply.github.com>
2024-08-05 09:39:24 +0200

    Update DockerService.sh

--- DockerService.sh
+++ DockerService.sh
@@ -2,6 +2,7 @@
 
 #Configure the Docker Service on host
 apt install python3-docker
+cd /tmp/
 cp mk_docker.py /usr/lib/check_mk_agent/plugins/
 cp docker.cfg /usr/lib/check_mk_agent/plugins/
 chmod 0755 /usr/lib/check_mk_agent/plugins/mk_docker.py
