d158988fb17211b078e935d5b1c725243948b354
Mauro García <159026290+magarpol@users.noreply.github.com>
2024-12-11 08:40:43 +0100

    Username Change
    List username and prompt for name changes, prompt as well for deleting users.

--- UsernameChange.sh
+++ UsernameChange.sh
@@ -14,11 +14,51 @@ change_username(){
     echo "User $old_username has been renamed to $new_username and home directory updated."
 }
 
-# Get the list of all users (exluding system account)
+# Function to delete old users with data zipping
+delete_user(){
+    local username=$1
+    local admin_home="/tmp"
+    local backup_dir"${admin_home}/user_backups"
+
+    mkdir -p "$backup_dir"  # Review backup directory exists
+
+    if id "$username" &>/dev/null; then
+        echo "Creating backup for $username..."
+        tar -czf "${backup_dir}/${username}.tar.gz" "/home/$username" &>/dev/null
+        echo "Backup created at ${backup_dir}/${username}.tar.gz"
+
+        echo "Deleting user $username..."
+        userdel -r "$username"
+        echo "User $username has been deleted."
+    else
+        echo "User $username does not exist."
+    fi
+}
+
+# Prevent locking user out
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root."
+    exit 1
+fi
+
+current_user=$(whoami)
+
+# Dry run
+dry_run_mode=false
+if[[ "$1" == "--dry-run" ]]; then
+    dry_run_mode=true
+    echo "Dry run mode enabled."
+fi
+
+# Get the list of all users (exluding system accounts)
 all_users=$(awk -F: '{ if ($3 >= 1000 && $3 != 65534) print $1 }' /etc/passwd)
 
 # Loop through all users
 for user in $all_users; do
+    if [[ "$user" == "$current_user" ]]; then
+        continue
+    fi
+
     echo -n "User $user new name?: "
     read new_name
 
@@ -28,7 +68,10 @@ for user in $all_users; do
         continue
     fi
 
-    # Perform the username change
+    # Perform the username change (dry run)
+    if $dry_run_mode; then
+        echo "Dry run: Changing username from $user to $new_name."
+    else
     change_username "$user" "$new_name"
 done
 
@@ -49,11 +92,15 @@ if [[ "$delete_choice" == "y" || "$delete_choice" == "Y" ]]; then
             break
         fi
 
-        if id "$user_to_delete" &>/dev/null: then
-            userdel -r "$user_to_delete"
-            echo "User $user_to_delete has been deleted."
-        else 
-            echo "User $user_to_delete does not exist."
+        if [[ $user_to_delete == "$current_user" ]]; then
+            echo "You cannot delete your own account ($current_user)."
+            continue
+        fi
+
+        if $dry_run_mode; then
+            echo "Dry run: Deleting user $user_to_delete."
+        else
+            delete_user "$user_to_delete"
         fi
     done
 fi
