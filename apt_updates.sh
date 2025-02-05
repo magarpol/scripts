826b72fce39b89473728232ee6a4e4336b4eb424
Mauro García <159026290+magarpol@users.noreply.github.com>
2025-02-05 10:27:41 +0100

    Create apt_updates.sh

---
+++ apt_updates.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ============================================================================
+# Script Name:    apt_updates.sh
+# Author:         Mauro García
+# Version:        2.1
+# Description:    This script configures APT updates service in CheckMK
+# Repository:     https://github.com/magarpol/scripts
+# Last Updated:   2025-01-30
+# ============================================================================
+
+#APT Update plugin in Linux servers
+cp /tmp/mk_apt /usr/lib/check_mk_agent/plugins/
+chmod 755 /usr/lib/check_mk_agent/plugins/mk_apt
+systemctl restart check-mk-agent.socket
