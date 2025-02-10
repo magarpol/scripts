645e347513920a246e3d9b17863038c2cdd866b6
Mauro García <159026290+magarpol@users.noreply.github.com>
2025-02-10 09:57:16 +0100

    Update docker_service.sh change data folder

--- docker_service.sh
+++ docker_service.sh
@@ -9,9 +9,9 @@
 # ============================================================================
 
 apt install python3-docker
-cd /tmp/
-cp mk_docker.py /usr/lib/check_mk_agent/plugins/
-cp docker.cfg /usr/lib/check_mk_agent/plugins/
+cd /scripts/data/
+cp /scripts/data/mk_docker.py /usr/lib/check_mk_agent/plugins/
+cp /scripts/data/docker.cfg /usr/lib/check_mk_agent/plugins/
 chmod 0755 /usr/lib/check_mk_agent/plugins/mk_docker.py
 cd /usr/lib/check_mk_agent/plugins/
 /usr/bin/python3 mk_docker.py
