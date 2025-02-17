1312cf37e29ca534cb37ad9c20a9abf3d5c4c2ac
Mauro García <159026290+magarpol@users.noreply.github.com>
2025-02-17 10:52:25 +0100

    Update update_docker_gpg_key.sh

--- update_docker_gpg_key.sh
+++ update_docker_gpg_key.sh
@@ -3,7 +3,7 @@
 # ============================================================================
 # Script Name:    update_docker_gpg_key.sh
 # Author:         Mauro García
-# Version:        1.0
+# Version:        2.0
 # Description:    This script updates the docker GPG key
 # Repository:     https://github.com/magarpol/scripts
 # Last Updated:   2025-02-10
@@ -13,28 +13,31 @@
 mkdir -p /usr/share/keyrings
 chmod 0755 /usr/share/keyrings
 
-# Move Docker GPG key to secure location
-mv /etc/apt/trusted.gpg.d/docker.gpg /usr/share/keyrings/docker-archive-keyring.gpg
-chmod 0644 /usr/share/keyrings/docker-archive-keyring.gpg
-
-# Find the correct Docker repository file
-DOCKER_LIST="/etc/apt/sources.list.d/docker.list"
-ARCHIVE_LIST=$(ls /etc/apt/sources.list.d/ | grep -E 'archive_uri-https_download_docker_com_linux_debian-bookworm.list' | head -n 1)
-
-# Update Docker repository file
-if [ -n "$DOCKER_LIST" ]; then
-    sed -i 's|deb \[arch=amd64\] |deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] |' "$DOCKER_LIST"
-    echo "Docker repository updated in $DOCKER_LIST."
-fi 
-
-# Update Archive repository file
-if [ -n "$ARCHIVE_LIST" ]; then
-    sed -i 's|deb \[arch=amd64\] |deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] |' "/etc/apt/sources.list.d/$ARCHIVE_LIST"
-    echo "Updated Docker repository in /etc/apt/sources.list.d/$ARCHIVE_LIST" 
+# Move Docker GPG key if the file exists
+if [ -f "/etc/apt/trusted.gpg.d/docker.gpg" ]; then
+    mv /etc/apt/trusted.gpg.d/docker.gpg /usr/share/keyrings/docker-archive-keyring.gpg
+    chmod 0644 /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "Moved Docker GPG key to /usr/share/keyrings/docker-archive-keyring.gpg"
+else
+    echo "Docker GPG key not found, skipping move."
 fi
 
-# Remove the old GPG key file
-rm -f /etc/apt/trusted.gpg
+# Find sources files that contains "download.docker.com"
+APT_FILES=$(grep -Rl "^deb.*download.docker.com" /etc/apt)
+
+if [ -n "$APT_FILES" ]; then
+    for FILE in $APT_FILES; do
+        # Backup the original file
+        cp "$FILE" "$FILE.bak"
+
+        # Modify the repository file
+        sed -i 's|deb \[arch=amd64\] |deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] |' "$FILE"
+
+        echo "Updated Docker repository in $FILE"
+    done
+else
+    echo "No Docker APT source found"
+fi 
 
 apt update
 
