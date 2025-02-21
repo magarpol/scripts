d0e379088471dbf0a8993d86bbeb2d7df4cf8d40
Mauro García <159026290+magarpol@users.noreply.github.com>
2025-02-21 07:49:39 +0100

    Update checkmk_services.sh

--- checkmk_services.sh
+++ checkmk_services.sh
@@ -13,8 +13,8 @@
 # Docker service #
 ##################
 apt install python3-docker
-cp /scripts/data/mk_docker.py /usr/lib/check_mk_agent/plugins/
-cp /scripts/data/docker.cfg /usr/lib/check_mk_agent/plugins/
+cp /root/scripts/data/mk_docker.py /usr/lib/check_mk_agent/plugins/
+cp /root/scripts/data/docker.cfg /usr/lib/check_mk_agent/plugins/
 chmod 0755 /usr/lib/check_mk_agent/plugins/mk_docker.py
 cd /usr/lib/check_mk_agent/plugins/
 /usr/bin/python3 mk_docker.py
@@ -24,7 +24,7 @@ echo "Docker service configured"
 ###############
 # APT Updates #
 ###############
-cp /scripts/data/mk_apt /usr/lib/check_mk_agent/plugins/
+cp /root/scripts/data/mk_apt /usr/lib/check_mk_agent/plugins/
 chmod 755 /usr/lib/check_mk_agent/plugins/mk_apt
 
 echo "APT Updates service configured"
@@ -32,7 +32,7 @@ echo "APT Updates service configured"
 ##################
 # Osinfo service # 
 ##################
-cp /scripts/data/osinfo /usr/lib/check_mk_agent/plugins/
+cp /root/scripts/data/osinfo /usr/lib/check_mk_agent/plugins/
 chmod +x /usr/lib/check_mk_agent/plugins/osinfo
 
 echo "Osinfo service configured"
@@ -40,7 +40,7 @@ echo "Osinfo service configured"
 ##################
 # Reboot service #
 ##################
-cp /scripts/data/reboot /usr/lib/check_mk_agent/plugins/
+cp /root/scripts/data/reboot /usr/lib/check_mk_agent/plugins/
 chmod +x /usr/lib/check_mk_agent/plugins/reboot
 
 echo "Reboot service configured"
